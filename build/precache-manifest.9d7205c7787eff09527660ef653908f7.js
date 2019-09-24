6c903335354b97b4772cce26107a874c604fd0e9
Jesús Torres <jesustc17@gmail.com>
2019-09-24 16:33:06 -0400

    agregado reactjs al proyecto

---
+++ build/precache-manifest.9d7205c7787eff09527660ef653908f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3765fd77135ccc8881b60064b98fa12f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc794dafbe68a27004d7",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f3863730fa7dc5667876",
+    "url": "/static/js/2.1c864a66.chunk.js"
+  },
+  {
+    "revision": "fc794dafbe68a27004d7",
+    "url": "/static/js/main.3499a0e3.chunk.js"
+  },
+  {
+    "revision": "cf59af692973bc9536b9",
+    "url": "/static/js/runtime-main.586173ff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- public/Main.js
+++ public/Main.js
@@ -4,8 +4,10 @@ function createWindow () {
   win = new BrowserWindow({width: 800, height: 600}) 
         
   // and load the index.html of the app.
-  win.loadFile('index.html')
+  // win.loadFile('index.html')
+  // win.loadFile('public/index.html')
 
+  win.loadURL('http://localhost:3000/')
   // win.webContents.openDevTools()
 }
 
